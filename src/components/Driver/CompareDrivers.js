c30c9755b4a085f0bf81e81fa412647cdc359773
st52557 <43813109+st52557@users.noreply.github.com>
2022-04-25 13:39:27 +0200

    Compare drivers edit

--- src/components/Driver/CompareDrivers.js
+++ src/components/Driver/CompareDrivers.js
@@ -1,18 +1,16 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, Component, useMemo} from "react";
 import {Col, Row, Container} from "react-bootstrap";
 import './Driver.scss'
 import {useAuth} from "../User/AuthContext";
-import SelectSearch from "react-select-search";
-
+import Select from 'react-select'
 
 function CompareDrivers() {
 
     const {token} = useAuth();
     const [drivers, setDrivers] = useState([]);
-    const [selected1, setSelected1] = useState("");
-    const [selected2, setSelected2] = useState("");
-    const [driver1, setDriver1] = useState({});
-    const [driver2, setDriver2] = useState({});
+    const [selected1, setSelected1] = useState({});
+    const [selected2, setSelected2] = useState({});
+    const [comparedDrivers, setComparedDrivers] = useState({});
     const [error, setError] = useState("");
 
     useEffect(() => {
@@ -35,58 +33,94 @@ function CompareDrivers() {
             // .then(response => response.json())
             .then(json => {
 
-                setDrivers((json.map(({id, name, surename}) => ({value: id, name: name + " " + surename}))));
+                setDrivers((json.map(({id, name, surename}) => ({value: id, label: name + " " + surename}))));
 
             })
             .catch((err) => setError(err.message))
 
     }, [])
 
-    const getDriver = (id, first) => {
-
+    const compareDrivers = () => {
+        fetch(`${process.env.REACT_APP_BASE_URI}/drivers-compare?firstDriverId=${encodeURIComponent(selected1.value)}&secondDriverId=${encodeURIComponent(selected2.value)}`,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': token
+                },
+            })
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+                throw new Error(`Unable to get data: ${response.statusText}`)
+            })
+            .then(json => {
+                setComparedDrivers(json);
+            })
+            .catch((err) => {
+                setError(err.message)
+            });
     }
+
+    useEffect(() => {
+
+        if (selected1.value && selected2.value) {
+            compareDrivers();
+        }
+
+    }, [selected1, selected2])
+
+
+    const selectSecondDriver = (selectedOption) => {
+        setSelected2(selectedOption);
+    };
+
+    const selectFirstDriver = (selectedOption) => {
+        setSelected1(selectedOption);
+    };
 
 
     return (
+
         <div>
-            <h1 style={{paddingTop: '2em'}}>Comparing drivers</h1>
+            <h1 style={{paddingTop: '2em',paddingBottom: '2em'}}>Comparing drivers</h1>
 
             <div className={"compare-drivers"}>
                 <Container>
                     <Row className={"justify-content-md-center"}>
                         <Col className={"compared-driver"}>
                             <div>
-                                {/*<SelectSearch*/}
-                                {/*    options={drivers}*/}
-                                {/*    search*/}
-                                {/*    placeholder="Select your country"*/}
-                                {/*/> */}
-                                <h4>1 -{driver1.name}</h4>
-
+                                <Select
+                                    options={drivers}
+                                    value={selected1}
+                                    onChange={selectFirstDriver}
+                                />
+                                <br/>
+                                {comparedDrivers &&
                                 <div>
-
-                                    {JSON.stringify(driver1, null, 2)}
-
+                                    <p>Overtakes: {comparedDrivers?.overtakes?.firstDriver}</p>
+                                    <p>Points: {comparedDrivers?.pointsScored?.firstDriver}</p>
+                                    <p>Wins: {comparedDrivers?.racesWon?.firstDriver}</p>
                                 </div>
+                                }
                             </div>
                         </Col>
                         <Col className={"compared-driver"}>
-                            <div >
-                                <SelectSearch
+                            <div>
+                                <Select
                                     options={drivers}
                                     value={selected2}
-                                    search
-                                    placeholder="Select second driver"
-                                    printOptions="auto"
-
+                                    onChange={selectSecondDriver}
                                 />
-                                <h4>2 - {driver2.name}</h4>
-
+                                <br/>
+                                {comparedDrivers &&
                                 <div>
-                                    {JSON.stringify(selected2, null, 2)}
-                                    {JSON.stringify(driver2, null, 2)}
-
+                                    <p>Overtakes: {comparedDrivers?.overtakes?.secondDriver}</p>
+                                    <p>Points: {comparedDrivers?.pointsScored?.secondDriver}</p>
+                                    <p>Wins: {comparedDrivers?.racesWon?.secondDriver}</p>
                                 </div>
+                                }
                             </div>
                         </Col>
                     </Row>
